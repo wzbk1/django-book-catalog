2735bf48882617a761587628008deb7f95a453cc
wzbk <stefanetsas1022007@gmail.com>
2025-02-03 23:32:12 +0200

    naprawianie blędów testowych

--- book_catalog/books/tests/test_acceptance.py
+++ book_catalog/books/tests/test_acceptance.py
@@ -2,16 +2,19 @@ from selenium import webdriver
 from django.test import LiveServerTestCase
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 class BookAcceptanceTest(LiveServerTestCase):
     def setUp(self):
-        self.driver = webdriver.Chrome()
+        service = Service(ChromeDriverManager().install())
+        self.driver = webdriver.Chrome(service=service)
 
     def tearDown(self):
         self.driver.quit()
 
     def test_add_book(self):
-        self.driver.get(self.live_server_url)
+        self.driver.get('http://127.0.0.1:8000')
         
         self.driver.find_element(By.CLASS_NAME, "btn-add-book").click()
         
@@ -24,3 +27,4 @@ class BookAcceptanceTest(LiveServerTestCase):
         self.assertIn("Test Book", body)
         self.assertIn("Test Author", body)
         self.assertIn("2025", body)
+        self.driver.quit()
